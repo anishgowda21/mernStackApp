888f168b3d87b4b8c2850a89059cde97a986dce4
Anish Gowda <anishgowda43@gmail.com>
2022-02-24 16:01:46 +0530

    added prod build settings

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const colors = require('colors');
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
@@ -16,5 +17,13 @@ app.use("/api/goals/",require("./routes/goalRoutes"))
 app.use("/api/users/",require("./routes/userRoutes"))
 
 app.use(errorHandler)
+
+if(process.env.NODE_ENV === 'prod'){
+    app.use(express.static(path.join(__dirname,'../frontend/build')))
+
+    app.get('*',(req,res) => res.sendFile(path.resolve(__dirname,'../','frontend','build','index.html')))
+} else {
+    app.get('/',(req,res) => res.send("Please set to production"))
+}
 
 app.listen(port,()=>console.log(`server started on port ${port}`))
